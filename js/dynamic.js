41ebe809c4d33fbab311f4bdcc449d5d877b277f
Russell Nyorere <ecodenhub@gmail.com>
2019-01-21 14:55:57 +0100

    Initial commit

---
+++ js/dynamic.js
@@ -0,0 +1,14 @@
+
+// const hamburger = document.getElementById('js-hamburger');
+// const navLinks = document.getElementById('js-nav-links');
+// navLinks.style.display = "none";
+
+// hamburger.addEventListener('click', () => {
+//     if (navLinks.style.display === "none") {
+//         navLinks.style.display = "block";
+//     } else {
+//         navLinks.style.display = "none";
+//     }
+// })
+
+
